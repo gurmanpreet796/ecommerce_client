b19f1d921ed9a03709e992f6de21fafe17565fc1
Gurman <kaurdhanjugurmnapreet@gmail.com>
2024-07-11 23:56:50 +0530

    Initial commit

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarousel/>
+      <div>
+        other section
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
